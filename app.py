622fbbcf25408c4a00b3fda4d6f761fb76afd722
gyang01 <georgiana.yang@yahoo.com>
2013-04-08 22:06:06 -0400

    moved stuff to correct directory ; added and fixed bugs in app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/logout')
 def logout():
-    session.pop():
+    session.pop()
     return redirect(url_for('index'))
 
 @app.route('/login')
@@ -25,9 +25,10 @@ def login():
             password=request.form['password']
             session['user']=username
             if not username in database.get_usernames():
-                return redirect(url_for('register')
-            return render_template("login.html",username,password)
+                return redirect(url_for('register'))  
+            return render_template("login.html",username=username,password=password)
             
+
 
 @app.route('/about')
 def about():
@@ -47,7 +48,13 @@ def register():
             osis=request.form['osis']
             digit=request.form['digit']
             exist=database.add_student(username,password,osis,digit)
-            return render_template("register.html",username,password,invalid,osis,digit,exist)
+            return render_template("register.html",
+                                   username=username,
+                                   password=password,
+                                   invalid=invalid,
+                                   osis=osis,
+                                   digit=digit,
+                                   exist=exist)
     return redirect(url_for(register))
 
 @app.route('/profile')
@@ -59,7 +66,11 @@ def profile():
         schedule=database.get_schedule(user)
         digit=database.get_id(user)
         osis=database.get_osis(user)
-        return render_template("profile.html",user,schedule,digit,osis)
+        return render_template("profile.html",
+                               user=user,
+                               schedule=schedule,
+                               digit=digit,
+                               osis=osis)
     elif request.method=='POST':
         if request.form['button']=='Set':
             return redirect(url_for("setschedule"))
